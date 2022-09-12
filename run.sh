16a3e78162b519671760c4bd31ea46ca515d57a2
albertlin <albert5718@gmal.com>
2022-09-12 22:29:48 +0800

    Fix sort issue

--- run.sh
+++ run.sh
@@ -37,7 +37,7 @@ cat ./$today/00915.csv >> ./$today/merged.csv
 cat ./$today/00713.csv >> ./$today/merged.csv
 
 sort -o ./$today/merged.csv ./$today/merged.csv
-awk -F, '{a[$1]++;b[$1]=$2;c[$1]+=$3}END{for(i in a){print i,b[i],c[i],a[i];}}' OFS="," ./$today/merged.csv | sort -r -n -t "," -k 3 -o ./$today/merged.csv
+awk -F, '{a[$1]++;b[$1]=$2;c[$1]+=$3}END{for(i in a){printf "%d,%s,%.2f,%d\n", i,b[i],c[i],a[i];}}' OFS="," ./$today/merged.csv | sort -r -n -t "," -k 3 -o ./$today/merged.csv
 
 # Find the stock is selected by only one fund, and keep it and slice the rest
 sed -i '/1$/q' ./$today/merged.csv
@@ -60,9 +60,12 @@ done
 paste -d "," ./$today/merged.csv ./$today/stock_close.csv > ./$today/tmp.csv
 mv ./$today/tmp.csv ./$today/merged.csv
 
+rm -f ./$today/stock_close.csv
+
 awk -F, '{s=800000*$5/100/$6/1000;printf"%.1f\n", s}' ./$today/merged.csv > ./$today/count.csv 
 paste -d "," ./$today/merged.csv ./$today/count.csv > ./$today/tmp.csv
 mv ./$today/tmp.csv ./$today/merged.csv
+rm -f ./$today/count.csv
 
 # Insert csv header
 sed  -i '1i Number,Name,Fund Rate,Count,Rate,Close Price,Amount' ./$today/merged.csv
