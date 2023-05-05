f5e977d23f5077415c49dc3aee9cbc68eaf4d162
albertlin <albertlin@ami.com.tw>
2023-05-06 00:31:35 +0800

    add explicit wait for 00878 completed page loading

--- 00878.py
+++ 00878.py
@@ -1,23 +1,35 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 # Desired URL
 url = "https://www.cathaysite.com.tw/fund-details/ECN?tab=portfolio"
 
 # create a new Firefox session
 driver = webdriver.Chrome()
-driver.implicitly_wait(60)
+driver.implicitly_wait(30)
 driver.get(url)
 
+try:
+    WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located
+            (
+                (By.XPATH,
+                    '//*[@id="portfolio"]/app-portfolio/div[1]/div/div[1]/div[2]/div[1]/ul')
+                )
+            )
+finally:
+
 # Pass to BS4
-soup = BeautifulSoup(driver.page_source, features="html.parser")
+    soup = BeautifulSoup(driver.page_source, features="html.parser")
 
 # Find all the div elements with the class 'content'
-# tds = soup.find_all("div", {"id":"portfolio"})
-tds = soup.find_all("ul", {"class":"bar-chart-ul ng-star-inserted"})
+    tds = soup.find_all("ul", {"class":"bar-chart-ul ng-star-inserted"})
 
 # Print the contents of the tds
-for td in tds:
-    print(td.text)
+    for td in tds:
+        print(td.text)
 
-driver.close()
+    driver.close()
